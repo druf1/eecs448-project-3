0fc95a5cd0527cc09b50240df1355e5d4e491103
JamesHansIV <jameshanselmaniv@ku.edu>
2022-03-27 16:06:37 -0500

    Added custom header that can be applied to all pages

---
+++ header.js
@@ -0,0 +1,18 @@
+class siteHeader extends HTMLElement {
+    connectedCallback() {   //called when this is appened to document
+        this.innerHTML = `
+            <img src="justBeClawsBanner.png" style="width:50%; height:auto; display:block; margin-left:auto; margin-right:auto"/>
+            <nav>
+                <ul>
+                    <li><a href="https://www.patreon.com/JustBeClaws">Donate</a></li>
+                    <li><a href="aboutUs.html">About Us</a></li>
+                    <li><a href="animals.html">Animals</a></li>
+                    <li><a href="signUp.html">Sign Up</a></li>
+                    <li><a href="signIn.html">Sign In</a></li>
+                </ul>
+            </nav>
+        `;
+    }
+}
+
+customElements.define('site-header',siteHeader);
